a7db27f1a8f996bfaee216c2407f5b2d8151ee71
Masika-codes <emasika215@gmail.com>
2024-07-29 15:23:28 +0300

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,21 @@
+"""(this is a multi-line comment)
+This is a code
+print ("Hello, user")
+name = input("name;")
+print ("Hello, " + name)
+#Another way to write statement two is; print (f"Hello, {name}")
+#Note the f=formatted string and use of curly braces
+
+"""
+
+#code no.2
+"""
+n = int (input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:#elif is short for else if
+    print("n is negative")
+else:
+    print("n is zero")
+"""
